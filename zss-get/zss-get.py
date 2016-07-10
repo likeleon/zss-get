83b750b2561d36fcf34c328b393f6109fdefd378
Sung Ho Kim <likeleon@gmail.com>
2016-07-10 13:42:44 +0900

    BeautifulSoup

--- zss-get/zss-get.py
+++ zss-get/zss-get.py
@@ -1,9 +1,39 @@
 import getopt
 import sys
+import logging
+import socket
 
 from util import log
+from urllib import request
+from bs4 import BeautifulSoup
 
 __version__ = '0.1'
+
+SITE = "http://zangsisi.net/"
+
+def get_content(url, headers={}, decoded=True):
+    logging.debug('get_content: %s' % url)
+
+    req = request.Request(url, headers=headers)
+
+    for i in range(10):
+        try:
+            response = request.urlopen(req)
+            break
+        except socket.timeout:
+            logging.debug('request attempt %s timeout' % str(i + 1))
+
+    data = response.read()
+
+    if decoded:
+        data = data.decode()
+
+    return data
+
+def list_books():
+    soup = BeautifulSoup(get_content(SITE), 'html.parser')
+    links = soup.find(id='recent-post').find_all('a', class_='tx-link')
+    print({ link.get('href'): link.get_text() for link in links })
 
 def main(**kwargs):
     def version():
@@ -15,13 +45,13 @@ def main(**kwargs):
     -h | --help                 Print help and exit.
     \n'''
     help += '''Dry-run options: (no actual downloading)
-    -i | --info                 Print extracted information.
+    -l | --list                 Display all available books.
     \n'''
 
-    short_opts = 'Vhi'
-    opts = ['version', 'help', 'info']
+    short_opts = 'Vhl'
+    opts = ['version', 'help', 'list']
 
-    sys.argv[1:] = ['-i']
+    sys.argv[1:] = ['-l']
     try:
         opts, args = getopt.getopt(sys.argv[1:], short_opts, opts)
     except getopt.GetoptError as err:
@@ -29,7 +59,6 @@ def main(**kwargs):
         log.e("try 'zss-get --help' for more options")
         sys.exit(2)
 
-    info_only = False
     for o, a in opts:
         if o in ('-V', '--version'):
             version()
@@ -38,8 +67,9 @@ def main(**kwargs):
             version()
             print(help)
             sys.exit()
-        elif o in ('-i', '--info'):
-            info_only = True
+        elif o in ('-l', '--list'):
+            list_books()
+            sys.exit()
         else:
             log.e("Try 'zss-get --help' for more options")
             sys.exit(2)
