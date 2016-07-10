3316da2f79fe836b9d05c3f0a8dedae68657e892
Sung Ho Kim <likeleon@gmail.com>
2016-07-11 00:42:38 +0900

    만화 필터

--- zss-get/zss-get.py
+++ zss-get/zss-get.py
@@ -3,7 +3,6 @@ import logging
 import socket
 import argparse
 
-from util import log
 from urllib import request
 from bs4 import BeautifulSoup
 
@@ -36,28 +35,36 @@ class Comic():
         self.url = url
         self.concluded = concluded
 
-def get_comics():
+def all_comics():
     soup = BeautifulSoup(get_content(SITE), 'html.parser')
     for a in soup.find(id='recent-post').find_all('a', class_='tx-link'):
         yield Comic(a.get_text(), a.get('href'), True)
     for a in soup.find(id='manga-list').find_all('a', class_='lists')[3:]:
         yield Comic(a.get_text(), a.get('href'), False)
 
-def list_comics():
-    for comic in get_comics():
-        print("     - title:     %s" % comic.title)
-        print("       url:       %s" % comic.url)
-        print("       concluded: %s" % comic.concluded)
+def print_comic(comic):
+    print("- title:     %s" % comic.title)
+    print("  url:       %s" % comic.url)
+    print("  concluded: %s" % comic.concluded)
+
+def get_books(comic):
+    soup = BeautifulSoup(get_content(comic.url), 'html.parser')
+    for a in soup.find(id='recent-post').find_all('a', class_='tx-link'):
+        yield a.get_text(), a.get('href')
+
+def download(comic):
+    for book, link in get_books(comic):
+        print("{}: {}".format(book, link))
 
 def get_parser():
     parser = argparse.ArgumentParser(description='zangsisi downloader')
-    parser.add_argument('book', metavar='BOOK', type=str, nargs='*', help='title of the book to download')
-    parser.add_argument('-l', '--list', help='display all available comics.', action='store_true')
+    parser.add_argument('keyword', metavar='KEYWORD', type=str, nargs='*', help='keyword for searching the book by its title')
+    parser.add_argument('-l', '--list', help='display all available comics', action='store_true')
     parser.add_argument('-v', '--version', help='displays the current version of zss-get', action='store_true')
     return parser
 
 def main(**kwargs):
-    #sys.argv[1:] = ['-l']
+    sys.argv[1:] = ['은혼']
 
     parser = get_parser()
     args = vars(parser.parse_args())
@@ -67,12 +74,22 @@ def main(**kwargs):
         return
 
     if args['list']:
-        list_comics()
+        for c in all_comics():
+            print_comic(c)
         return
 
-    if not args['book']:
+    if not args['keyword']:
         parser.print_help()
         return
+
+    comics = [c for c in all_comics() if all(k in c.title for k in args['keyword'])]
+    if len(comics) > 1:
+        print("Ambiguous keywords: '{}'. Matched comics: ".format(", ".join(keywords)))
+        for c in comics:
+            print_comic(c)
+        return
+
+    download(comics[0])
 
 if __name__ == '__main__':
     main()
